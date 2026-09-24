a6920d4f3515281d6232db54b37cc54e249e2d13
Daniel <danieldelgadomoreno0@gmail.com>
2026-09-24 15:18:10 +0200

    Practica5

---
+++ TIPOS_DE_DATOS/Practica5.py
@@ -0,0 +1,4 @@
+usuario = input("Cual es tu usuario? ")
+horas = int(input("Cuantas horas has trabajado? "))
+costH = int(input("Cuanto ganas la hora? "))
+print (f"Hola {usuario} se te comunica que has ganado", horas * costH)
